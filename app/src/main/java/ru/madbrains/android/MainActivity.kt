82913d607010696410fd7346897d0f14a0c11827
shadowik <zahar.mark@bk.ru>
2022-03-04 15:51:24 +0400

    Done

--- app/src/main/java/ru/madbrains/android/MainActivity.kt
+++ app/src/main/java/ru/madbrains/android/MainActivity.kt
@@ -1,29 +1,31 @@
 package ru.madbrains.android
 
-import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.util.Log
 import android.widget.ImageView
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.R
 import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var id_image: Int = 1
+    lateinit var imageView: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.textId.text = "Hello World"
-        binding.imageView.setImageResource(R.drawable.one)
+
+        imageView = binding.imageView
 
         val resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
                 result ->
-                val data: Intent? = result.data
-                setImage(data?.getIntExtra("imgId", 1), binding.imageView)
+                val data = result.data?.getIntExtra("imgId", 1) ?: 1
+                setImage(data, binding.imageView)
         }
         val intent = Intent(this, MainActivity2::class.java)
         binding.button.setOnClickListener() {
@@ -31,7 +33,8 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun setImage(id: Int?, imageView: ImageView) {
+    fun setImage(id: Int, imageView: ImageView) {
+        id_image = id
         Log.d("test", id.toString())
         when (id) {
             1 -> imageView.setImageResource(R.drawable.one)
@@ -39,9 +42,17 @@ class MainActivity : AppCompatActivity() {
             3 -> imageView.setImageResource(R.drawable.three)
             else -> Log.d("test", "Error")
         }
+
     }
 
-    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
-        super.onSaveInstanceState(outState, outPersistentState)
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.d("test", "IIIIIIIIIIIIIIIIIII")
+        outState.putInt("ImageId", id_image)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        setImage(savedInstanceState.getInt("ImageId"), imageView)
     }
 }
